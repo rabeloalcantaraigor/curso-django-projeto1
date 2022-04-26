61ec61faec80be6feeb40532b955e59f132faa06
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 <rabelo.alcantara.igor@gmail.com>
2022-04-26 15:18:52 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -5,8 +5,4 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/home.html')
 
-def contato(request):
-    return HttpResponse('contato')
 
-def sobre(request):
-    return HttpResponse('sobre')
