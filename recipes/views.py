6fdf9184732576a244310f920e26368aac76873f
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 <rabelo.alcantara.igor@gmail.com>
2022-04-25 22:04:52 -0300

    template html

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('HOME')
+
+def contato(request):
+    return HttpResponse('contato')
+
+def sobre(request):
+    return HttpResponse('sobre')
